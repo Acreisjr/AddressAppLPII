98471fea5ca5e47db5b5b0ea4f7f18bb5db62635
Beto <acreisjunio@gmail.com>
2019-11-30 08:53:20 -0300

    Foi oque eu consegui :(

--- AddressApp/AddressApp/src/ch/makery/address/model/PersonListWrapper.java
+++ AddressApp/AddressApp/src/ch/makery/address/model/PersonListWrapper.java
@@ -13,21 +13,11 @@ public class PersonListWrapper {
 
     private List<Person> persons;
 
-    /**
-     * Retorna uma Lista de Pessoas.
-     *
-     * @return persons
-     */
     @XmlElement(name = "person")
     public List<Person> getPersons() {
         return persons;
     }
 
-    /**
-     * Define uma Lista de Pessoas.
-     *
-     * @param persons
-     */
     public void setPersons(List<Person> persons) {
         this.persons = persons;
     }
